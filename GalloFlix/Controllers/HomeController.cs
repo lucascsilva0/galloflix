3583b504d50de3a5e4b154c5d66ed440dd70c679
Lucas Silva <lucascattosilva2018@gmail.com>
2023-05-29 09:37:25 -0300

    Estudos de Orientação a Objetos.

--- GalloFlix/Controllers/HomeController.cs
+++ GalloFlix/Controllers/HomeController.cs
@@ -4,28 +4,39 @@ using GalloFlix.Models;
 
 namespace GalloFlix.Controllers;
 
-public class HomeController : Controller
-{
+public class HomeController : Controller{
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
+    public HomeController(ILogger<HomeController> logger){
         _logger = logger;
     }
 
-    public IActionResult Index()
-    {
-        return View();
+    public IActionResult Index(){
+        List<Genre> genres = new(){
+            new Genre(){
+                Id   = 1,
+                Name = "Ação"
+            },
+            new Genre(){
+                Id   = 2,
+                Name = "Terror"
+            }
+        }; // objeto genres é uma lista de gêneros
+        genres.Add(
+            new(){
+                Id   = 3,
+                Name = "Drama"
+            }
+        );
+        return View(genres);
     }
 
-    public IActionResult Privacy()
-    {
+    public IActionResult Privacy(){
         return View();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
+    public IActionResult Error(){
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
--- GalloFlix/Models/Genre.cs
+++ GalloFlix/Models/Genre.cs
@@ -1,6 +1,8 @@
 
 namespace GalloFlix.Models;
 public class Genre{
+
     public byte   Id   { get; set; }
+
     public string Name { get; set; }
 }
